dec1fe386186bd45ac9b2a603921dc4396fdf55c
tetsuya-takayanagi-09 <tetsuya.takayanagi09@gmail.com>
2026-02-22 16:52:28 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </StrictMode>
 );
